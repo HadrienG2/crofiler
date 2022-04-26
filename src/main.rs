5eab513bea694ac2dadb44801c0a822b98ed95f6
Hadrien Grasland <grasland@lal.in2p3.fr>
2022-04-26 17:41:29 +0200

    Saner display

--- src/main.rs
+++ src/main.rs
@@ -163,7 +163,7 @@ pub struct Sample {
     pub weight: SampleWeight,
 }
 
-/// CPU identifier
+/// CPU identifier (following libc)
 pub type CpuId = i32;
 
 /// Thread ID (following libc)
@@ -182,7 +182,12 @@ pub struct EventCategories(Vec<String>);
 //
 impl From<String> for EventCategories {
     fn from(s: String) -> Self {
-        Self(s.split(',').map(|sub| sub.to_owned()).collect())
+        Self(
+            s.split(',')
+                .filter(|sub| sub.len() > 0)
+                .map(|sub| sub.to_owned())
+                .collect(),
+        )
     }
 }
 
@@ -193,8 +198,35 @@ fn main() {
         .unwrap()
         .read_to_string(&mut s)
         .unwrap();
-    let value = json::from_str::<TraceData>(&s).unwrap();
-    println!("{:#?}", value);
+    let value = json::from_str::<TraceDataObject>(&s).unwrap();
+
+    let mut display_period = 1;
+    let display_period_increment = 10;
+    let display_increment_period = 10;
+    let mut displayed_events = 0;
+    let mut current_event = 0;
+    for e in value.traceEvents {
+        match e {
+            TraceEvent::M(_) => println!("{:#?}", e),
+            TraceEvent::X { .. } => {
+                if current_event % display_period == 0 {
+                    if (displayed_events > 0) && (display_period > 1) {
+                        println!("... skipped {display_period} complete events ...");
+                    }
+                    println!("{:#?}", e);
+                    displayed_events += 1;
+                    if displayed_events % display_increment_period == 0 {
+                        display_period *= display_period_increment;
+                        println!(
+                            "... will now only display 1 in {display_period} complete events ..."
+                        );
+                    }
+                }
+                current_event += 1;
+            }
+            _ => panic!("Unexpected event type"),
+        }
+    }
 }
 
 #[cfg(test)]
