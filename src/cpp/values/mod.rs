fce2136cf48dbb1095629bcc4d0387ab7198e444
Hadrien Grasland <grasland@lal.in2p3.fr>
2022-05-18 11:51:28 +0200

    Add support for parenthesized values

--- src/cpp/values/mod.rs
+++ src/cpp/values/mod.rs
@@ -12,13 +12,15 @@ use nom::Parser;
 /// Parser recognizing values (and some values that are indistinguishable from
 /// values without extra context)
 pub fn value_like(s: &str) -> IResult<ValueLike> {
+    use nom::{character::complete::char, sequence::delimited};
+    let value_like = || value_like.map(Box::new);
     let literal = literals::literal.map(ValueLike::Literal);
-    let unary_op = operators::unary_expr_prefix
-        .and(value_like.map(Box::new))
+    let parentheses = delimited(char('('), value_like(), char(')')).map(ValueLike::Parentheses);
+    let unary_op = (operators::unary_expr_prefix.and(value_like()))
         .map(|(op, expr)| ValueLike::UnaryOp(op, expr));
     // TODO: Should accept id-expressions here, but not necessary yet since they
     //       are accepted by TypeLike as well.
-    literal.or(unary_op).parse(s)
+    literal.or(parentheses).or(unary_op).parse(s)
 }
 //
 /// A value, or something that looks close enough to it
@@ -27,7 +29,10 @@ pub enum ValueLike<'source> {
     /// Literal
     Literal(Literal<'source>),
 
-    /// Unary operator AST
+    /// Value with parentheses around it
+    Parentheses(Box<ValueLike<'source>>),
+
+    /// Unary operator applied to a value
     UnaryOp(Operator<'source>, Box<ValueLike<'source>>),
 }
 //
@@ -45,11 +50,16 @@ mod tests {
 
     #[test]
     fn value_like() {
+        assert_eq!(super::value_like("'@'"), Ok(("", '@'.into())));
+        assert_eq!(
+            super::value_like("(42)"),
+            Ok(("", ValueLike::Parentheses(Box::new(42u8.into()))))
+        );
         assert_eq!(
             super::value_like("&123"),
             Ok((
                 "",
-                ValueLike::UnaryOp(Symbol::AndRef.into(), Box::new((123i8).into()))
+                ValueLike::UnaryOp(Symbol::AndRef.into(), Box::new((123u8).into()))
             ))
         );
     }
