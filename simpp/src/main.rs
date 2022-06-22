b93305c1259bf0d802ae6d0e44f88ddd84d5c5ee
Hadrien Grasland <hadrien.grasland@gmx.fr>
2022-06-22 11:13:33 +0200

    First demo of a display with a terminal column budget

--- simpp/src/main.rs
+++ simpp/src/main.rs
@@ -3,8 +3,10 @@ use std::{fmt::Display, io::Read};
 use clap::Parser;
 use cpparser::{
     display::{CustomDisplay, DisplayState},
-    EntityParser,
+    EntityParser, EntityView,
 };
+use std::fmt::Write;
+use unicode_width::UnicodeWidthStr;
 
 /// Simplify a C++ entity name into a human-readable form
 ///
@@ -14,33 +16,26 @@ use cpparser::{
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
 struct Args {
-    /// Recursion depth
-    #[clap(long)]
-    recursion_depth: usize,
+    /// Terminal column budget
+    #[clap(short, long)]
+    cols: usize,
 
     /// C++ entity name to be simplified (can also be specified via stdin)
     entity: Option<String>,
 }
+
+/// Helper to display a C++ entity with a custom configuration
+struct EntityDisplay<'entities> {
+    /// C++ entity to be displayed
+    entity: &'entities EntityView<'entities>,
+
+    /// Initial display state
+    state: DisplayState,
+}
 //
-impl Display for Args {
+impl Display for EntityDisplay<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        // Extract input string
-        let entity = self
-            .entity
-            .as_ref()
-            .expect("Please provide input via either CLI or stdin");
-
-        // Parse C++ entity name
-        let parser = EntityParser::new();
-        let entity = parser
-            .parse_entity(&entity)
-            .expect("Failed to parse entity name");
-        let entities = parser.finalize();
-        let entity = entities.entity(entity);
-
-        // Display it
-        println!("Recursion depth is {:#?}", entity.recursion_depth());
-        entity.display(f, &DisplayState::new(self.recursion_depth))
+        self.entity.display(f, &self.state)
     }
 }
 
@@ -50,17 +45,49 @@ fn main() {
 
     // Get C++ entity name from either CLI or stdin
     let mut stdin = String::new();
-    args.entity = args.entity.take().or_else(|| {
-        if atty::isnt(atty::Stream::Stdin) {
-            std::io::stdin()
-                .read_to_string(&mut stdin)
-                .expect("Failed to read entity from standard input");
-            Some(stdin)
-        } else {
-            None
-        }
-    });
+    let entity = args
+        .entity
+        .take()
+        .or_else(|| {
+            if atty::isnt(atty::Stream::Stdin) {
+                std::io::stdin()
+                    .read_to_string(&mut stdin)
+                    .expect("Failed to read entity from standard input");
+                Some(stdin)
+            } else {
+                None
+            }
+        })
+        .expect("Please provide a C++ entity name via either CLI or stdin");
+
+    // Parse C++ entity name
+    let parser = EntityParser::new();
+    let entity = parser
+        .parse_entity(&entity)
+        .expect("Failed to parse entity name");
+    let entities = parser.finalize();
+    let entity = entities.entity(entity);
 
     // Display it
-    println!("{args}");
+    let mut prev_display = "…".to_string();
+    let mut curr_display = String::new();
+    for recursion_depth in 0..entity.recursion_depth() {
+        write!(
+            &mut curr_display,
+            "{}",
+            EntityDisplay {
+                entity: &entity,
+                state: DisplayState::new(recursion_depth)
+            }
+        )
+        .unwrap();
+        if curr_display.width() > args.cols {
+            break;
+        } else {
+            prev_display.clear();
+            prev_display.clone_from(&curr_display);
+            curr_display.clear()
+        }
+    }
+    println!("{prev_display}");
 }
