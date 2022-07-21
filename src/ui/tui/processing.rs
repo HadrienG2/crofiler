7acdc9beb4923a28741b1a6b80c9fd24f9999782
Hadrien Grasland <grasland@lal.in2p3.fr>
2022-07-21 15:13:24 +0200

    Add basic tree queries

--- src/ui/tui/processing.rs
+++ src/ui/tui/processing.rs
@@ -1,7 +1,7 @@
 //! Processing thread of the TUI interface (owns the ClangTrace and takes care
 //! of all the expensive rendering operations to allow good responsiveness).
 
-use clang_time_trace::{ClangTrace, ClangTraceLoadError};
+use clang_time_trace::{ActivityTrace, ActivityTraceId, ClangTrace, ClangTraceLoadError, Duration};
 use std::{
     path::Path,
     sync::{
@@ -9,7 +9,7 @@ use std::{
         Arc, Mutex, TryLockError,
     },
     thread::{self, JoinHandle},
-    time::Duration,
+    time,
 };
 
 /// Encapsulation of the processing thread
@@ -22,6 +22,9 @@ pub struct ProcessingThread<Metadata: Send + Sync + 'static> {
 
     /// Channel to send instructions to the processing thread
     instruction_sender: Sender<Instruction>,
+
+    /// Channel to receive lists of activities from the processing thread
+    activities_receiver: Receiver<Box<[ActivityInfo]>>,
 }
 //
 impl<Metadata: Send + Sync + 'static> ProcessingThread<Metadata> {
@@ -35,6 +38,7 @@ impl<Metadata: Send + Sync + 'static> ProcessingThread<Metadata> {
         let metadata = Arc::new(Mutex::new(None));
         let metadata2 = metadata.clone();
         let (instruction_sender, instruction_receiver) = mpsc::channel();
+        let (activities_sender, activities_receiver) = mpsc::channel();
 
         // Spawn the processing thread
         let handle = thread::spawn(move || {
@@ -54,21 +58,23 @@ impl<Metadata: Send + Sync + 'static> ProcessingThread<Metadata> {
             };
 
             // Process instructions until the main thread hangs up
-            worker(trace, instruction_receiver);
+            worker(trace, instruction_receiver, activities_sender);
         });
 
         // Wait for the thread to start processing the ClangTrace (as indicated
-        // by acquisition of the metadata lock or insertion of metadata)
-        while matches!(
-            *metadata.try_lock().expect("Processing thread has crashed"),
-            None
-        ) {
-            std::thread::sleep(Duration::new(0, 1_000));
+        // by acquisition of the metadata lock or emission of metadata)
+        while metadata
+            .try_lock()
+            .expect("Processing thread has crashed")
+            .is_none()
+        {
+            thread::sleep(time::Duration::new(0, 10_000));
         }
         Self {
             handle,
             metadata,
             instruction_sender,
+            activities_receiver,
         }
     }
 
@@ -94,16 +100,103 @@ impl<Metadata: Send + Sync + 'static> ProcessingThread<Metadata> {
             }
         }
     }
+
+    // TODO: Consider making the following requests asynchronous for increased
+    //       UI responsiveness once basic functionality is achieved. This will
+    //       require attaching a counter to replies which allows telling apart
+    //       the reply to one query from that to another query.
+
+    /// Get the list of root nodes
+    pub fn root_activities(&self) -> Box<[ActivityInfo]> {
+        self.query(Instruction::GetRootNodes);
+        Self::fetch(&self.activities_receiver)
+    }
+
+    /// Get the list of a node's children
+    pub fn direct_children(&self, id: ActivityTraceId) -> Box<[ActivityInfo]> {
+        self.query(Instruction::GetChildren(id));
+        Self::fetch(&self.activities_receiver)
+    }
+
+    /// The processing thread should keep listening to instructions as long as
+    /// the main thread is active, otherwise it's strongly indicative of a crash
+    fn query(&self, instruction: Instruction) {
+        self.instruction_sender
+            .send(instruction)
+            .expect("Processing thread has likely crashed")
+    }
+
+    /// The processing thread should keep replying to instructions as long as
+    /// the main thread is active, otherwise it's strongly indicative of a crash
+    fn fetch<T: Send + 'static>(receiver: &Receiver<T>) -> T {
+        receiver
+            .recv()
+            .expect("Processing thread has likely crashed")
+    }
+}
+
+/// Basic activity data as emitted by the processing thread
+///
+/// This is meant to provide enough info to allow for pruning based on the
+/// desired minimal duration criteria (e.g. self_duration > 0.5% total), before
+/// querying for the name info which is more expensive to render.
+///
+pub struct ActivityInfo {
+    /// Identifier that can be used to refer to the activity in later requests
+    pub id: ActivityTraceId,
+
+    /// Time spent processing this activity or one of its callees
+    pub duration: Duration,
+
+    /// Time spent specificially processing this activity
+    pub self_duration: Duration,
+}
+
+/// Instructions that can be sent to the processing thread
+enum Instruction {
+    /// Get the list of root nodes (reply via activities channel)
+    GetRootNodes,
+
+    /// Get the list of a node's children (reply via activities channel)
+    GetChildren(ActivityTraceId),
 }
 
 /// Processing thread worker
-fn worker(trace: ClangTrace, instructions: Receiver<Instruction>) {
+fn worker(
+    trace: ClangTrace,
+    instructions: Receiver<Instruction>,
+    activities: Sender<Box<[ActivityInfo]>>,
+) {
+    // Process instructions until the main thread hangs up
     while let Ok(instruction) = instructions.recv() {
         match instruction {
-            () => {}
+            // Get the list of root nodes
+            Instruction::GetRootNodes => reply(&activities, activity_list(trace.root_activities())),
+
+            // Get the list of a node's children
+            Instruction::GetChildren(id) => reply(
+                &activities,
+                activity_list(trace.activity(id).direct_children()),
+            ),
         }
     }
 }
 
-// TODO: Here be an enum of possible instructions
-type Instruction = ();
+/// When the main thread asks for something, it should wait for the reply before
+/// quitting, otherwise it's strongly indicative of a crash.
+fn reply<T: Send + 'static>(sender: &Sender<T>, data: T) {
+    sender.send(data).expect("Main thread has likely crashed")
+}
+
+/// Build a list of activities
+fn activity_list<'a>(iterator: impl Iterator<Item = ActivityTrace<'a>>) -> Box<[ActivityInfo]> {
+    iterator
+        .map(|activity_trace| ActivityInfo {
+            id: activity_trace.id(),
+            duration: activity_trace.duration(),
+            self_duration: activity_trace.self_duration(),
+        })
+        .collect()
+}
+
+// FIXME: Add tests
