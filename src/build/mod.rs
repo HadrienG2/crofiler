97c2b4b5627e29a71f6b0796217fc0c193b326e5
Hadrien Grasland <hadrien.grasland@gmx.fr>
2022-09-28 05:55:19 +0200

    Add a file age probe

--- src/build/mod.rs
+++ src/build/mod.rs
@@ -3,3 +3,28 @@
 mod clang;
 mod commands;
 mod profile;
+
+use std::{
+    io,
+    path::Path,
+    time::{Duration, SystemTimeError},
+};
+use thiserror::Error;
+
+/// Measure how log ago a file was last modified
+fn file_age(path: impl AsRef<Path>) -> Result<Duration, FileAgeError> {
+    let age = std::fs::metadata(path)?.modified()?.elapsed()?;
+    Ok(age)
+}
+
+/// Error while looking up the age of a file
+#[derive(Debug, Error)]
+enum FileAgeError {
+    /// Failed to access the file
+    #[error("failed to access the file ({0})")]
+    BadIo(#[from] io::Error),
+
+    /// Queried system time looks wrong
+    #[error("modification timestamp is from the future ({0})")]
+    BadClock(#[from] SystemTimeError),
+}
