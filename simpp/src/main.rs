0a2e1dcb63aab370e4addc2fafc3e14dacdc38a6
Hadrien G <knights_of_ni@gmx.com>
2023-07-03 21:51:30 +0200

    Replace abandoned atty crate with is_terminal

--- simpp/src/main.rs
+++ simpp/src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use cpparser::{display::CustomDisplay, EntityParser};
+use is_terminal::IsTerminal;
 use std::io::Read;
 
 /// Simplify a C++ entity name into a human-readable form
@@ -28,13 +29,13 @@ fn main() {
         .entity
         .take()
         .or_else(|| {
-            if atty::isnt(atty::Stream::Stdin) {
+            if std::io::stdin().is_terminal() {
+                None
+            } else {
                 std::io::stdin()
                     .read_to_string(&mut stdin)
                     .expect("Failed to read entity from standard input");
                 Some(stdin)
-            } else {
-                None
             }
         })
         .expect("Please provide a C++ entity name via either CLI or stdin");
