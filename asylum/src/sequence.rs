7e8f35c8aa7aaf1cbe60fe3c47743b916bd7779c
Hadrien Grasland <hadrien.grasland@gmx.fr>
2024-10-19 12:32:44 +0200

    Apply rustfmt

--- asylum/src/sequence.rs
+++ asylum/src/sequence.rs
@@ -232,9 +232,7 @@ pub struct SequenceEntry<
     initial_concatenated_len: usize,
 }
 //
-impl<Item: Clone + Eq + Hash, K: InternerKey<ImplKey = Range<usize>>>
-    SequenceEntry<'_, Item, K>
-{
+impl<Item: Clone + Eq + Hash, K: InternerKey<ImplKey = Range<usize>>> SequenceEntry<'_, Item, K> {
     /// Add an item to the sequence that is being interned
     pub fn push(&mut self, item: Item) {
         self.interner.concatenated.push(item);
